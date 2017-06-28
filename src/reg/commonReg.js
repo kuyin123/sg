b9d0e098a8118681c201961da0ed36bb23feb8b5
q1cha0 <cqc168@live.com>
2017-06-28 16:26:42 +0800

    updata

--- src/reg/commonReg.js
+++ src/reg/commonReg.js
@@ -3,13 +3,38 @@
  */
 
 // 常用正则判断
-// 1. 手机号: 必须以1开头的十一位数字
-const regPhoneNo = /^1\d{10}$/;
+// 传入要验证的字符串, 返回 Boolean 值.
+
+// 1. 手机号: 1开头的十一位数字
+var regPhoneNo = function (str) {
+	return /^1\d{10}$/.test(str);
+}
 
 // 2. 邮箱地址
-const regEMail = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+var regEMail = function (str) {
+	return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(str);
+}
+
+// 3. 用户名: 字母、数字、下划线，1-16位
+var regUserName = function (str) {
+	return /^\w{1,16}$/.test(str);
+}
+
+// 4. 用户密码: 密码必须由数字、字母、特殊字符组成且密码长度为1~16个字符
+var regPassword = function (str) {
+	// return /^[A-Za-z0-9_#@$%^&*()~]{1,16}$/.test(str);
+	return /^[\w#@$%^&*()~]{1,16}$/.test(str);
+}
+
+// 5. 标准日期时间格式(yy/mm/dd hh:mm:ss 或者 yy-mm-dd hh:mm:ss)判断
+var regDateFormat = function (str) {
+	return /^\d{4,}(\/|-)(?:0?\d|1[12])(\/|-)(?:[012]?\d|3[01]) (?:[01]?\d|2[0-4]):(?:[0-5]?\d|60):(?:[0-5]?\d|60)$/.test(str);
+}
 
 export default {
 	regPhoneNo,
-	regEMail
+	regEMail,
+	regUserName,
+	regPassword,
+	regDateFormat
 }
