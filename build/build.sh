da200ccc23c2a223efbe19626a786be57a6b7e34
kuyin123 <328178972@qq.com>
2017-06-28 17:56:39 +0800

    更改编译脚本的错误

--- build/build.sh
+++ build/build.sh
@@ -5,9 +5,5 @@ basePath=$(cd `dirname $0`; pwd)
 cd ${basePath}/../
 rm -r dist
 
-npm run prepublish
-
 ./node_modules/.bin/webpack
 ./node_modules/.bin/webpack -p
-./node_modules/.bin/webpack --config extension/webpack.config.js
-./node_modules/.bin/webpack --config extension/webpack.config.js -p
