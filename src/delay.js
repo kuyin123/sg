3d91e76e1c119bd7f86158275ecd2342eb2b10c6
q1cha0 <cqc168@live.com>
2017-06-28 11:20:39 +0800

    add dir

--- src/delay.js
+++
@@ -1,8 +0,0 @@
-function delay(func, wait, ...args) {
-  if (typeof func != 'function') {
-    throw new TypeError('Expected a function')
-  }
-  return setTimeout(func, +wait || 0, ...args);
-}
-
-export default delay
--- src/index.js
+++
@@ -1,2 +0,0 @@
-module.exports = require('./arrayEach')
-require("./delay.js")
