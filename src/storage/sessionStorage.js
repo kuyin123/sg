cc3c47a84c65d4b224be62e9254c17ebdd768f59
hyb508 <539969654@qq.com>
2017-06-29 15:24:06 +0800

    修改文件

--- src/storage/sessionStorage.js
+++ src/storage/sessionStorage.js
@@ -1,18 +1,12 @@
-if (!window.sessionStorage) {
-  throw '浏览器不支持sessionStorage';
-};
-var storage = window.sessionStorage,
-  store, _api, even_storage = function() {};
-
-function isJSON(obj) {
+function _isJSON(obj) {
   return typeof(obj) === "object" && Object.prototype.toString.call(obj).toLowerCase() === "[object object]" && !obj.length;
 }
 
-function stringify(val) {
+function _stringify(val) {
   return val === undefined || typeof val === "function" ? val + '' : JSON.stringify(val);
 }
 
-function deserialize(value) {
+function _deserialize(value) {
   if (typeof value !== 'string') {
     return undefined;
   }
@@ -23,50 +17,22 @@ function deserialize(value) {
   }
 }
 
-function isFunction(value) {
-  return ({}).toString.call(value) === "[object Function]";
-}
-
-function isArray(value) {
-  return value instanceof Array;
-}
-
-function dealIncognito(storage) {
-  var _KEY = '_Is_Incognit',
-    _VALUE = 'yes';
-  try { storage.setItem(_KEY, _VALUE) } catch (e) {
-    if (e.name === 'QuotaExceededError') {
-      var _nothing = function() {};
-      storage.__proto__ = { setItem: _nothing, getItem: _nothing, removeItem: _nothing, clear: _nothing };
-    }
-  } finally {
-    if (storage.getItem(_KEY) === _VALUE) storage.removeItem(_KEY);
+class Store {
+  constructor() {
+    this.storage = window.sessionStorage
   }
-  return storage;
-}
-
-storage = dealIncognito(storage);
-
-function Store() {
-  if (!(this instanceof Store)) {
-    return new Store();
-  }
-}
-
-Store.prototype = {
-  set: function(key, val) {
-    even_storage('set', key, val);
-    if (key && !isJSON(key)) {
-      storage.setItem(key, stringify(val));
-    } else if (key && isJSON(key) && !val) {
-      for (var a in key) this.set(a, key[a]);
+  set(key, val) {
+    if (key && !_isJSON(key)) {
+      this.storage.setItem(key, _stringify(val));
+    } else if (key && _isJSON(key) && !val) {
+      for (let a in key) this.set(a, key[a]);
     }
-    return this;
-  },
-  get: function(key) {
+    return this
+  }
+  get(key) {
     if (!key) {
-      var ret = {};
-      this.forEach(function(key, val) {
+      let ret = {};
+      this._forEach(function(key, val) {
         ret[key] = val;
       });
       return ret;
@@ -74,51 +40,35 @@ Store.prototype = {
     if (key.charAt(0) === '?') {
       return this.has(key.substr(1));
     }
-    return deserialize(storage.getItem(key));
-  },
-  clear: function() {
-    this.forEach(function(key, val) {
-      even_storage('clear', key, val);
-    });
-    storage.clear();
+    return _deserialize(this.storage.getItem(key));
+  }
+  clear() {
+    this.storage.clear();
     return this;
-  },
-  remove: function(key) {
-    var val = this.get(key);
-    storage.removeItem(key);
-    even_storage('remove', key, val);
+  }
+  remove(key) {
+    let val = this.get(key);
+    this.storage.removeItem(key);
     return val;
-  },
-  has: function(key) {
+  }
+  has(key) {
     return ({}).hasOwnProperty.call(this.get(), key);
-  },
-  keys: function() {
+  }
+  keys() {
     var d = [];
-    this.forEach(function(k, list) {
+    this._forEach(function(k, list) {
       d.push(k);
     });
     return d;
-  },
-  size: function() {
+  }
+  size() {
     return this.keys().length;
-  },
-  forEach: function(callback) {
-    for (var i = 0; i < storage.length; i++) {
-      var key = storage.key(i);
+  }
+  _forEach(callback) {
+    for (var i = 0; i < this.storage.length; i++) {
+      var key = this.storage.key(i);
       if (callback(key, this.get(key)) === false) break;
     }
-    return this;
-  },
-  search: function(str) {
-    var arr = this.keys(),
-      dt = {};
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i].indexOf(str) > -1) dt[arr[i]] = this.get(arr[i]);
-    }
-    return dt;
-  },
-  onStorage: function(cb) {
-    if (cb && isFunction(cb)) even_storage = cb;
     return this;
   }
 }
