a2a97406e3a395e2c342dc202bdaba723d40c1a6
hellovue2017 <gouxiaojun@shenguyun.com>
2017-06-29 09:23:19 +0800

    update

--- src/commonFun/localStorage.js
+++ src/commonFun/localStorage.js
@@ -3,7 +3,9 @@
  * */
 let storage = (function () {
     if (window.localStorage) {
+
         let ls = window.localStorage;
+
         //更新缓存数据
         function setLocalStorage(name, val) {
             let str = typeof name === 'string' || typeof name === "number";
@@ -12,6 +14,7 @@ let storage = (function () {
             str && !obj && ls.setItem(name, val);
             str && obj && ls.setItem(name, JSON.stringify(val));
         };
+
         //获取缓存数据
         function getLocalStorage(name) {
             let item = ls.getItem(name);
@@ -21,19 +24,23 @@ let storage = (function () {
                 return item;
             }
         };
+
         //删除缓存数据
         function removeLocalStorage(name) {
             return ls.removeItem(name);
         };
+
         //清除所有缓存数据
         function clearAllLocalStorage() {
             ls.clear();
         };
+
         return {
             setLocalStorage,
             getLocalStorage,
             clearAllLocalStorage
         }
+
     } else {
         throw new Error('浏览器不支持LocalStorage对象。。。。。。。');
     }
